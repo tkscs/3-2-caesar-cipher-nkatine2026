925201f215305019b80f335e796d5ab158144292
nkatine2026 <nkatine2026@kehillahstudent.org>
2026-02-03 10:24:03 -0800

    donzo

--- main.py
+++ main.py
@@ -6,29 +6,30 @@ alphabet = string.ascii_lowercase
 print(f"{alphabet = }")
 
 # You can look up the index of a letter in the alphabet like this:
-index = alphabet.index("a")
-print(f"position of 'a' in the alphabet: {index}")
+index = alphabet.index("n")
+print(f"position of 'n' in the alphabet: {index}")
 
 # The computer already thinks of all the characters it can print out as numbers.
 # If you want to, you can look up what number a character corresponds to in
 # "ASCII" encoding:
-ascii_number = ord("a")
-print(f"ascii number representation of 'a': {ascii_number}")
+ascii_number = ord("n")
+print(f"ascii number representation of 'n': {ascii_number}")
 
-ascii_letter = chr(97)
-print(f"ascii letter at position #97: {ascii_letter}")
+ascii_letter = chr(4)
+print(f"ascii letter at position #4: {ascii_letter}")
 
 # ----- Your Algorithm -----
 
 # Your task is to encrypt this secret message into ciphertext
-plaintext = "This is a secret message."
+plaintext = "nein"
 
 # Initialize your ciphertext an empty string
 ciphertext = ""
 for character in plaintext:
-    # do something to the character to encrypt it
-    # YOUR CODE HERE
-    encrypted_character = "a" # CHANGE THIS!
+    if character in "aejou":
+        encrypted_character = "n"
+    else:
+        encrypted_character = character
     ciphertext += encrypted_character
 
 print(f"{ciphertext = }")
